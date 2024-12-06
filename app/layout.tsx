818b03fbec1de4d713019786c9cb07278fff0bb4
Se4Eagle <shaamil.is@gmail.com>
2024-12-06 20:03:54 +0500

    build the navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { NavBar } from "./NavBar";
+import { Main } from "next/document";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,10 +27,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <NavBar></NavBar>
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   );
