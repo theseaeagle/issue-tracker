ee36b4472c441732e32ea6538023e0fc6242f7ae
Se4Eagle <shaamil.is@gmail.com>
2024-12-06 23:26:54 +0500

    style the active link in the nav bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,12 @@
+'use client';
 import Link from 'next/link'
 import React from 'react'
 import { FaBug } from 'react-icons/fa'
+import { usePathname } from 'next/navigation'
+import  classNames   from 'classnames'
 
 export const NavBar = () => {
+    const currentPath = usePathname();
     const links = [
         {label: 'Dashboard',href: '/'},
         {label: 'Issues',href: '/issues'},
@@ -13,8 +17,14 @@ export const NavBar = () => {
         <ul className='flex space-x-6'>
             {links.map(link => <Link 
                 key={link.href}
+                className = {classNames({
+                    'text-zinc-900': link.href==currentPath,
+                    'text-zinc-500': link.href!== currentPath,
+                    'hover:text-zinc-800 transition-colors': true
+
+                })}
                 href={link.href}
-                className="text-zinc-500; hover:text-zinc-800 transition-colors">{link.label} </Link>
+                >{link.label} </Link>
             )}
             
         </ul>
