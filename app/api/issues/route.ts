6ed27b0f3e0db89b19d3e42e965215ca106f210b
Se4Eagle <shaamil.is@gmail.com>
2024-12-07 09:21:07 +0500

    handle errors in issue submit form

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -10,7 +10,7 @@ export async function POST(request:NextRequest) {
     const body =  await request.json()
     const validation = createIssueSchema.safeParse(body);
     if(!validation.success){
-        return NextResponse.json({error:validation.error.errors},{status:400})
+        return NextResponse.json({error:validation.error.format()},{status:400})
     }
     
     const newIssue = await prisma.issues.create({
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,64 @@
 'use client';
-import { Box, Button, Container, TextArea, TextField, ThemePanel } from '@radix-ui/themes'
-import React from 'react'
+import { Box, Button, Callout, Container, TextArea, TextField, ThemePanel } from '@radix-ui/themes';
+import React, { useState } from 'react';
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import { InfoCircledIcon } from '@radix-ui/react-icons';
+
+
 
 const NewIssuePage = () => {
+    const router = useRouter();
+    type Inputs = {
+        title: string
+        description: string
+      }
+
+    const {
+        register,
+        control,
+        handleSubmit,
+        watch,
+        formState: { errors },
+      } = useForm<Inputs>()
+
+      const [error,setError] = useState('');
   return (
     <>
-    <div className='max-w-xl px-5 space-y-3'>
-        <TextField.Root placeholder="Search the docs…"></TextField.Root>
-        <SimpleMDE />
-        <Button>Add Issue</Button>
-        
+    <div>
+        {error &&
+            <div className='max-w-xl px-5 space-y-3 my-5'>
+                <Callout.Root color="red" >
+                    <Callout.Icon>
+                        <InfoCircledIcon />
+                    </Callout.Icon>
+                    <Callout.Text>
+                        {error}
+                    </Callout.Text>
+                </Callout.Root>
+            </div>
+        }
+        <form className='max-w-xl px-5 space-y-3' onSubmit={handleSubmit(async (data)=> {
+            try {
+                await axios.post('/api/issues',data); 
+                router.push('/issues');
+            } catch (error) {
+                setError('An unexpected error occured!')
+            }
+            
+            })}>
+            <TextField.Root {...register("title", { required: true })} placeholder="Search the docs…"></TextField.Root>
+            <Controller 
+            name = 'description'
+            control = {control}
+            render = {({field})=><SimpleMDE {...field}/>}
+            ></Controller>
+            <Button type="submit">Add Issue</Button>
+        </form>    
     </div>
     </>
   )
