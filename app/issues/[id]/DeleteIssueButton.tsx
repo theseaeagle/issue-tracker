3d9b0bf9409c9f044ec718d9edd4e1575dfe94d3
Se4Eagle <shaamil.is@gmail.com>
2024-12-11 17:51:24 +0500

    handle errors

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 import * as AlertDialog from "@radix-ui/react-alert-dialog";
-import React from "react";
+import React, { useState } from "react";
 import prisma from "@/prisma/client";
 import { Button } from "@radix-ui/themes";
 import axios from "axios";
@@ -10,46 +10,81 @@ const DeleteIssueButton = ({ issueid }: { issueid: number }) => {
   //prisma.issues.delete(
   //where:{id:issueid}
   // )
+  const deleteIssue = async () => {
+    try {
+      //throw new Error();
+      await axios.delete("/api/issues/" + issueid);
+      router.push("/issues");
+      router.refresh();
+    } catch {
+      setError(true);
+    }
+  };
   const router = useRouter();
+  const [error, setError] = useState(false);
   return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger asChild>
-        <button className="inline-flex h-[35px] items-center justify-center rounded bg-white px-[15px] font-medium leading-none text-violet11 shadow-[0_2px_10px] shadow-blackA4 outline-none hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black">
-          Delete account
-        </button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Portal>
-        <AlertDialog.Overlay className="fixed inset-0 bg-blackA6 data-[state=open]:animate-overlayShow" />
-        <AlertDialog.Content className="fixed left-1/2 top-1/2 max-h-[85vh] w-[90vw] max-w-[500px] -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none data-[state=open]:animate-contentShow">
-          <AlertDialog.Title className="m-0 text-[17px] font-medium text-mauve12">
-            Are you absolutely sure?
-          </AlertDialog.Title>
-          <AlertDialog.Description className="mb-5 mt-[15px] text-[15px] leading-normal text-mauve11">
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </AlertDialog.Description>
-          <div className="flex justify-end gap-[25px]">
-            <AlertDialog.Cancel asChild>
-              <button className="inline-flex h-[35px] items-center justify-center rounded bg-mauve4 px-[15px] font-medium leading-none text-mauve11 outline-none hover:bg-mauve5 focus:shadow-[0_0_0_2px] focus:shadow-mauve7">
-                Cancel
-              </button>
-            </AlertDialog.Cancel>
-            <AlertDialog.Action asChild>
-              <button
-                onClick={async () => {
-                  await axios.delete("/api/issues/" + issueid);
-                  router.push("/issues");
-                  router.refresh();
-                }}
-                className="inline-flex h-[35px] items-center justify-center rounded bg-red4 px-[15px] font-medium leading-none text-red11 outline-none hover:bg-red5 focus:shadow-[0_0_0_2px] focus:shadow-red7"
-              >
-                Yes, delete account
-              </button>
-            </AlertDialog.Action>
-          </div>
-        </AlertDialog.Content>
-      </AlertDialog.Portal>
-    </AlertDialog.Root>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger asChild>
+          <button className="inline-flex h-[35px] items-center justify-center rounded bg-white px-[15px] font-medium leading-none text-violet11 shadow-[0_2px_10px] shadow-blackA4 outline-none hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black">
+            Delete account
+          </button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Portal>
+          <AlertDialog.Overlay className="fixed inset-0 bg-blackA6 data-[state=open]:animate-overlayShow" />
+          <AlertDialog.Content className="fixed left-1/2 top-1/2 max-h-[85vh] w-[90vw] max-w-[500px] -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none data-[state=open]:animate-contentShow">
+            <AlertDialog.Title className="m-0 text-[17px] font-medium text-mauve12">
+              Are you absolutely sure?
+            </AlertDialog.Title>
+            <AlertDialog.Description className="mb-5 mt-[15px] text-[15px] leading-normal text-mauve11">
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialog.Description>
+            <div className="flex justify-end gap-[25px]">
+              <AlertDialog.Cancel asChild>
+                <button className="inline-flex h-[35px] items-center justify-center rounded bg-mauve4 px-[15px] font-medium leading-none text-mauve11 outline-none hover:bg-mauve5 focus:shadow-[0_0_0_2px] focus:shadow-mauve7">
+                  Cancel
+                </button>
+              </AlertDialog.Cancel>
+              <AlertDialog.Action asChild>
+                <button
+                  onClick={deleteIssue}
+                  className="inline-flex h-[35px] items-center justify-center rounded bg-red4 px-[15px] font-medium leading-none text-red11 outline-none hover:bg-red5 focus:shadow-[0_0_0_2px] focus:shadow-red7"
+                >
+                  Yes, delete account
+                </button>
+              </AlertDialog.Action>
+            </div>
+          </AlertDialog.Content>
+        </AlertDialog.Portal>
+      </AlertDialog.Root>
+
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Portal>
+          <AlertDialog.Overlay className="fixed inset-0 bg-blackA6 data-[state=open]:animate-overlayShow" />
+          <AlertDialog.Content className="fixed left-1/2 top-1/2 max-h-[85vh] w-[90vw] max-w-[500px] -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none data-[state=open]:animate-contentShow">
+            <AlertDialog.Title className="m-0 text-[17px] font-medium text-mauve12">
+              Error
+            </AlertDialog.Title>
+            <AlertDialog.Description className="mb-5 mt-[15px] text-[15px] leading-normal text-mauve11">
+              Issue could not be deleted
+            </AlertDialog.Description>
+            <div className="flex justify-end gap-[25px]">
+              <AlertDialog.Cancel asChild>
+                <button
+                  onClick={() => {
+                    setError(false);
+                  }}
+                  className="inline-flex h-[35px] items-center justify-center rounded bg-mauve4 px-[15px] font-medium leading-none text-mauve11 outline-none hover:bg-mauve5 focus:shadow-[0_0_0_2px] focus:shadow-mauve7"
+                >
+                  OK
+                </button>
+              </AlertDialog.Cancel>
+            </div>
+          </AlertDialog.Content>
+        </AlertDialog.Portal>
+      </AlertDialog.Root>
+    </>
   );
 };
 
